d49e3d4c5ad4654cd540fbcebba50783bd2d6f02
John Pavlick <jmpavlick@gmail.com>
2026-01-28 14:03:03 -0500

    master: savepoint

---
+++ schema.ts
@@ -0,0 +1,23 @@
+import z from "zod"
+
+const inputConfigSchema = z.object({})
+
+const outputConfigSchema = z.object({
+  dir: z.string().describe("file path for output, relative to the `workingDir` option"),
+  mkdirP: z.boolean().describe("create the directory path for `outputDir` if it does not exist"),
+  cleanOnGenerate: z
+    .boolean()
+    .describe("clean the output directory before `typewriter` generates output"),
+})
+
+export const configSchema = z.object({
+  workingDir: z
+    .union([
+      z.literal("here"),
+      z.literal("root"),
+      z.string().describe("file path relative to the configuration file"),
+    ])
+    .describe("working directory for `typewriter` execution"),
+  input: inputConfigSchema,
+  output: outputConfigSchema,
+})
