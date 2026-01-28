c3c09468a946362a097fbd80bdd3eec44375f2d0
John Pavlick <jmpavlick@gmail.com>
2026-01-28 11:57:21 -0500

    : savepoint

---
+++ nx.ts
@@ -0,0 +1,26 @@
+import z from "zod"
+import stringify from "safe-stable-stringify"
+
+let objSchemas: Record<string, z.ZodObject> = {}
+
+const simpleObj = z.object({
+  name: z.string(),
+  age: z.int(),
+  weight: z.number(),
+  optStr: z.string().optional(),
+  nullableStr: z.string().nullable(),
+  nullishStr: z.string().nullish()
+
+})
+
+objSchemas.simpleObj = simpleObj
+
+Array.from(Object.entries(objSchemas)).forEach(([name, sch]) => {
+  const shape = sch.shape
+
+  console.log(stringify({ [name]: shape }, null, 2))
+})
+
+const strSch = z.string()
+
+console.log(stringify({ strSch }, null, 2))
