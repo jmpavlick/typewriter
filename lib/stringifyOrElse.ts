f909dfd068c054bdf4fe3676fcc4622ff5f7439f
John Pavlick <jmpavlick@gmail.com>
2026-01-29 10:28:58 -0500

    master: savepoint

--- lib/stringifyOrElse.ts
+++ lib/stringifyOrElse.ts
@@ -4,4 +4,4 @@ import { ResultAsync, Result, okAsync } from "neverthrow"
 type Params = Parameters<typeof stringify>
 
 export default (...params: Params) =>
-  okAsync().andThen(() => Result.fromThrowable(stringify)(params))
+  okAsync().andThen(() => Result.fromThrowable(stringify)(...params))
--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import "../lib/serializeErrorPrototype.js"
+import "./lib/serializeErrorPrototype.js"
 import stringify from "safe-stable-stringify"
 import stringifyOrElse from "./lib/stringifyOrElse.js"
 import { type ResultAsync, okAsync, errAsync, fromPromise, Result } from "neverthrow"
@@ -6,37 +6,88 @@ import dedent from "dedent"
 import z from "zod"
 import zx from "./lib/zod/ext.js"
 import { ternary } from "./lib/neverthrow/ext.js"
+import * as fs from "fs"
+import { dirname } from "path"
 
 // eventually, make this stuff configurable
-const WORKDIR = "./.typewriter"
+const ROOT = `.`
+const WORKDIR = `${ROOT}/.typewriter`
+const INPUT = `${ROOT}/tests/schemaVariants.ts`
+const OUTFILE_CODEGEN_INPUT = `${WORKDIR}/${INPUT.replace(WORKDIR, "")}.json`
 
-const zodDeclSchema = z.record(
+const toWorkdirPath = (root: string) => `${ROOT}/.typewriter`
+const toInput = (root: string) => (relInputPath: string) => ({
+  path: `${root}/${relInputPath}`,
+  rel: relInputPath,
+})
+
+const toOutfileCodegenInput = (root: string) => (input: { rel: string }) =>
+  `${toWorkdirPath(root)}/${input.rel}.json`
+
+const toConfig = (root: string, relativeInputPath: string) => {
+  const input = toInput(root)(relativeInputPath)
+  return {
+    workdirPath: toWorkdirPath(root),
+    input,
+    outfileCodegenInputPath: toOutfileCodegenInput(root)(input),
+  }
+}
+
+const config = toConfig(".", "tests/schemaVariants.ts")
+
+export type Config = ReturnType<typeof toConfig>
+
+// read the input file as a module and do cursed shit to make it more objectionable
+// (pun intended)
+const read = (filepath: string): ResultAsync<Record<string, unknown>, unknown> =>
+  fromPromise(import(filepath), (e) => `Failed to import from filepath: ${e}`).andThen(
+    Result.fromThrowable((m) => Object.fromEntries(Object.entries(m)))
+  )
+
+// transform the module-object to zod schemas
+const zodDeclsSchema = z.record(
   z.string(),
-  z.custom<z.ZodType>((v) => v instanceof z.ZodType, { error: "Value must be a Zod schema" })
+  z.custom<z.ZodType>(
+    (v) => {
+      // console.log(v)
+      return v instanceof z.ZodType
+    },
+    { error: "Value must be a Zod schema" }
+  )
 )
 
-// ResultAsync<Record<string, z.ZodType>, string>
-const toZodSchemas = (module: unknown): ResultAsync<Record<string, z.ZodType>, unknown> =>
+type ZodDecls = z.infer<typeof zodDeclsSchema>
+
+const toZodSchemas = (module: unknown): ResultAsync<ZodDecls, unknown> =>
   ternary((m) => typeof m === "object" && m !== null)
     .whenFalse(errAsync(`Dynamic import failed; module contains no exports`))(module)
-    .andThen(zx.parseResultAsync(zodDeclSchema))
+    .andThen(zx.parseResultAsync(zodDeclsSchema))
 
-// (typeof module === "object" && module !== null
-//   ? okAsync<object, string>(module)
-//   : errAsync(`Imported module did not contain any exports`)
-// )
-// .map((module) => {
-//   const decls: [string, z.ZodType][] = Object.entries(module).filter(
-//     ([_, value]) => value instanceof z.ZodType
-//   )
+// get the girl, put the girl in the barn
+// (the american dream)
+const toCodgenInputFile = (path: string) => (zds: ZodDecls) =>
+  stringifyOrElse(zds, null, 2)
+    .andTee((v) => console.log(v))
+    .andThrough(() =>
+      Result.fromThrowable(() => fs.mkdirSync(dirname(path), { recursive: true }))()
+    )
+    .andThen(Result.fromThrowable((str) => fs.writeFileSync(path, str)))
 
-//   return decls
-// })
+// the chain that fleetwood mac said they'd never break
+const run = (config: Config) =>
+  read(config.input.path)
+    .andThen(toZodSchemas)
+    .andThen(toCodgenInputFile(config.outfileCodegenInputPath))
 
-// const read = (filepath: string): ResultAsync<string, string> =>
-//   fromPromise(
-//     import(filepath),
-//     (e) => `Failed to import from filepath: ${filepath}:\n\n{stringify(e)}`
-//   ).andThen(toZodSchemas)
-
-// const writeElmCodegenInput = async (data: unknown) => {}
+// for dev testing, just do it
+run(config)
+  .andTee((output) => {
+    console.log(stringify(output, null, 2))
+    console.log()
+    console.log("OK")
+  })
+  .orTee((output) => {
+    console.error(stringify(output, null, 2))
+    console.error()
+    console.error("ERR")
+  })
