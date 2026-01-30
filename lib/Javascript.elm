19d1e6a87604fb5e09de5f33436d1bf93b4c2086
John Pavlick <jmpavlick@gmail.com>
2026-01-30 16:58:49 -0500

    master: savepoint

---
+++ lib/Javascript.elm
@@ -0,0 +1,21 @@
+module Javascript exposing (..)
+
+{-| a ridiculous module in a ridiculous codebase
+-}
+
+import Json.Decode as D
+
+
+{-| louder than god's revolver
+, and twice as shiny
+-}
+type NaN
+    = NaNaNaNaNaNaNaNaNaNaNaNaNaNaNaNaNaNaNaNaNaNaNaNaNaNaNaNaNaNaNaNaNaNaNaNaNaNaNaNaNaNaNa
+
+
+
+-- nanDecoder : D.Decoder NaN
+-- nanDecoder =
+--     D.oneOf
+--         [ D.
+--         ]
