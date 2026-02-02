11d5b922e2617aa3af2b7406fed425161d4ec7fa
John Pavlick <jmpavlick@gmail.com>
2026-02-02 15:00:47 -0500

    master: fixup for install

--- src/init.ts
+++ src/init.ts
@@ -4,7 +4,7 @@ import { ResultAsync } from "neverthrow"
 
 export const init = (): ResultAsync<void, unknown> => {
   const __dirname = fs.toModuleDir(import.meta.url)
-  const templatesDir = path.join(__dirname, "..", "templates")
+  const templatesDir = path.join(__dirname, "..", "..", "templates")
 
   return fs.cwd().andThen((targetDir) =>
     ResultAsync.combineWithAllErrors([
