5b2975d39a8632e30e6e294c32b20977fb9ef5db
John Pavlick <jmpavlick@gmail.com>
2026-02-02 09:37:07 -0500

    master: savepoint

--- index.ts
+++ index.ts
@@ -1,27 +1,10 @@
 import "./lib/serializeErrorPrototype.js"
-import { type Config, toConfig } from "./src/config.js"
+import { type Config } from "./src/config.js"
 import * as ElmCodegen from "./lib/elmCodegen.js"
-import zx from "./lib/zod/ext.js"
-import z from "zod"
-import { ResultAsync, okAsync, errAsync } from "neverthrow"
 import path from "path"
-import * as Astify from "./src/astify.js"
-import { doAsync } from "./lib/neverthrow/ext.js"
-import * as fs from "./lib/fs.js"
+import { type RunProps } from "./src/main.js"
 
-/** a `Config` describes all of the program's executions; `RunProps` describes a single execution of the program
-
-  a `Config` becomes many `RunProps`es
- */
-const runProps = z.object({
-  label: z.string(),
-  inputPath: z.string(),
-  elmCodegenConfig: ElmCodegen.config,
-  debugZodAstOutputPath: z.string(),
-})
-type RunProps = z.infer<typeof runProps>
-
-const toRunPropsEntries = ({
+export const toRunPropsEntries = ({
   root,
   elmCodegenConfig: globalElmCodegenConfig,
   sections,
@@ -55,15 +38,3 @@ const toRunPropsEntries = ({
 
   return Array.from(Object.entries(sections)).flatMap(fromSection)
 }
-
-const run = ({ label, inputPath, elmCodegenConfig, debugZodAstOutputPath }: RunProps) =>
-  doAsync(() => {
-    console.log(`section: ${label}\ninput: ${inputPath}`)
-  })
-    .andThen(() => Astify.execute(inputPath))
-    .andThrough((zodDecls) =>
-      elmCodegenConfig.debug
-        ? fs.writeFile(debugZodAstOutputPath, JSON.stringify(zodDecls, null, 2))
-        : okAsync(undefined)
-    )
-    .andThen(ElmCodegen.execute(elmCodegenConfig))
---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import * as ElmCodegen from "../lib/elmCodegen.js"
+import z from "zod"
+import { ResultAsync, okAsync } from "neverthrow"
+import * as Astify from "./astify.js"
+import { doAsync } from "../lib/neverthrow/ext.js"
+import * as fs from "../lib/fs.js"
+
+/** a `Config` describes all of the program's executions; `RunProps` describes a single execution of the program
+
+  a `Config` becomes many `RunProps`es
+ */
+export const runProps = z.object({
+  label: z.string(),
+  inputPath: z.string(),
+  elmCodegenConfig: ElmCodegen.config,
+  debugZodAstOutputPath: z.string(),
+})
+export type RunProps = z.infer<typeof runProps>
+
+export const run = ({ label, inputPath, elmCodegenConfig, debugZodAstOutputPath }: RunProps) =>
+  doAsync(() => {
+    console.log(`section: ${label}\ninput: ${inputPath}`)
+  })
+    .andThen(() => Astify.execute(inputPath))
+    .andThrough((zodDecls) =>
+      elmCodegenConfig.debug
+        ? fs.writeFile(debugZodAstOutputPath, JSON.stringify(zodDecls, null, 2))
+        : okAsync(undefined)
+    )
+    .andThen(ElmCodegen.execute(elmCodegenConfig))
