7f0add86a4f72d294837c1298284e61248cdc19d
John Pavlick <jmpavlick@gmail.com>
2026-02-02 15:32:39 -0500

    master: savepoint

--- src/ouroboros.ts
+++ src/ouroboros.ts
@@ -1,7 +1,12 @@
 import { type ConfigParams, toConfig } from "./config.js"
+import * as fs from "../lib/fs.js"
+import path from "path"
+
+const __dirname = fs.toModuleDir(import.meta.url)
+const packageRoot = path.join(__dirname, "..")
 
 const configParams: ConfigParams = {
-  root: ".",
+  root: packageRoot,
   sections: {
     main: {
       relativeInputPaths: ["src/config.ts"],
